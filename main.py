858041b6ec8677c9234cb5854f2427b323fef93c
esuldin <>
2024-01-13 03:55:17 +0100

    Remove unused file

--- main.py
+++
@@ -1,7 +0,0 @@
-from game_window_manager import MsMinesweeperWindowManager
-from game_field import MsMinesweeperClassicField
-
-window_manager = MsMinesweeperWindowManager()
-field = MsMinesweeperClassicField(window_manager)
-
-print(field._field)
